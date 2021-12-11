a8f65d8d34492d81871a997730bc20c63d6b1502
Aeveq <95948421+Aeveq@users.noreply.github.com>
2021-12-10 18:11:45 -0800

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+sudo su
+read password
+
+if [$EUID -ne 0]
+then
+    echo Script not run as root
+    exit
+fi
+
+mkdir -p ~/Desktop/backup
+chmod 777 ~/Desktop/backup
+echo Backup Folder Successful
+
+cp /etc/group ~/Desktop/backup
+cp /etc/passwd ~/Desktop/backup
+echo Backup /etc/group & /etc/passwd Successful
+
+echo Enter all users required by CyberPatriot, separated by a space
+read -a users
+
+usersLen = ${#users[@]}
+
+for ((i = 0; i < $usersLen; i++)) do
+    clear
+    echo ${users[$i]}
+    echo Delete ${users[$i]}? yes or no
+    read del
+    if [[ $del == yes ]] then
+        deluser ${users[$i]}
+        echo ${#users[@]} deleted
+    else
+        echo Is ${#users[@]} admin or user?
+        read adm
+        if [[ $adm == admin ]] then
+            adduser ${#users[@]} adm
+            echo ${#users[@]} is now an admin
+        elif [[ $adm == user ]] then
+            deluser ${#users[@]} adm
+            echo ${#users[@]} is now a user
+        fi
+        echo Set password for user? yes or no
+        read ynpswd
+        if [[ $ynpswd == yes ]] then
+            echo Password:
+            read pw
+            $pw | passwd ${#users[@]}
+            echo ${#users[@]} password changed to $pw
+        else
+            echo ${#users[@]} password unchanged
+        fi
+    fi
+done
